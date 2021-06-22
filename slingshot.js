b84ff6dc8f46853569012b670bfeaf14ef990e7c
neha-11coder <83291551+neha-11coder@users.noreply.github.com>
2021-06-22 19:55:15 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,35 @@
+class Slingshot{
+
+constructor(bodyA,bodyB){
+    var options={
+    bodyA:bodyA,
+    bodyB:bodyB,
+    stiffness:0.04,
+    length:10
+}
+this.Slingshot=Constraint.create(options);
+World.add(world,this.Slingshot);
+}
+
+
+display(){
+var pointA=this.Slingshot.bodyA.position;
+var pointB=this.Slingshot.bodyB.position;
+stroke("pink")
+strokeWeight(3);
+line (pointA.x,pointA.y,pointB.x,pointB.y);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
